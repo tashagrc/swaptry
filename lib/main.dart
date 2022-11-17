719855addf6cf5160d3693ca27a41d99663b35a3
lixklelik <lixkelik@gmail.com>
2022-11-17 23:21:05 +0700

    connecting firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:swaptry/carbonara_swaptry_icons.dart';
 import 'page/home_page.dart';
 import 'page/history_page.dart';
 import 'page/profile_page.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MainPage());
 }
 
