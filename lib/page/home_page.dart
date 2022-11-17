c7858267d234aef6048ab677b091789e1b0c5022
lixklelik <lixkelik@gmail.com>
2022-11-18 02:00:55 +0700

    homepage update

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +11,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) => MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'Poppins'
+    ),
     home: Scaffold(
-      body: Center(child: Text('HomePage', style: TextStyle(fontSize: 60))),
-      
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Color(0xff565656),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.search),
+
+                SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    'Search swap station', 
+                    style: TextStyle(
+                      color: Color(0xff565656),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xfff5f5f5),
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+        ),
+      ),
+
+      body: Stack(
+        children: [
+          Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset('assets/img/home_rectangle.png', fit: BoxFit.fill),
+          ),
+        ],
+      ),
     ),
   );
 }
