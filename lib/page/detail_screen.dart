3855efe6b45aa722e65443368e23fc203096b208
Natasha Grace <natashagradika@gmail.com>
2022-11-19 16:33:43 +0700

    detail

--- lib/page/detail_screen.dart
+++ lib/page/detail_screen.dart
@@ -5,6 +5,106 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+      child: Stack(
+        children: [
+          Image.asset(
+            'assets/img/jakselstation.png',
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            // bulet2 nav yg atas
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                    onTap: () {
+                      Navigator.pop(context, true);
+                    },
+                    child: Image.asset('assets/btn_back.png', width: 40))
+              ],
+            ),
+          ),
+          ListView(
+            children: [
+              SizedBox(height: 244),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 30,
+                        horizontal: 24,
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 8,
+                          ),
+                          // text header
+                          Text(
+                            'Jaksel Station',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            'Tebet Barat, Jakarta Selatan',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                          // status yg ijo
+                          Image.asset(
+                            'asset/img/status.png',
+                            width: 60,
+                          ),
+                          Text(
+                            'Directions',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            '1.2 Km From you',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                          // batteries type
+                          Row(
+                            children: [],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
