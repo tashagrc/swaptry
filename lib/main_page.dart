4c88730106b3fec3b6a996016ebcfd5612a7823e
Natasha Grace <natashagradika@gmail.com>
2022-12-03 22:34:10 +0700

    test

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:swaptry/page/auth_page.dart';
+import 'package:swaptry/page/landing_page.dart';
 import 'package:swaptry/page/widgets/navigation.dart';
 import 'package:swaptry/page/widgets/utils.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
-
-
 
 class MainPageRoute extends StatefulWidget {
   const MainPageRoute({super.key});
@@ -16,9 +15,6 @@ class MainPageRoute extends StatefulWidget {
 }
 
 class _MainPageRouteState extends State<MainPageRoute> {
-
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,15 +24,14 @@ class _MainPageRouteState extends State<MainPageRoute> {
       scaffoldMessengerKey: Utils.messengerKey,
       navigatorKey: navigatorKey,
       home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if(snapshot.hasData){
-            return const Navigation();
-          }else{
-            return const AuthPage();
-          }
-        }
-      ),
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Navigation();
+            } else {
+              return const LandingPage();
+            }
+          }),
     );
   }
 }
--- lib/page/landing_page.dart
+++ lib/page/landing_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:swaptry/page/auth_page.dart';
+import 'package:swaptry/page/widgets/appTheme.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -11,8 +13,67 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(110, 128, 254, 1),
-      appBar: AppBar(),
+      backgroundColor: purple,
+      body: Stack(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              width: 360,
+              height: 560,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/img/welcome_bar.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                // const Text(
+                //   textAlign: TextAlign.center,
+                //   "Let's start our journey\nshall we?",
+                // ),
+                // FittedBox(
+                //   child: GestureDetector(
+                //     onTap: () {
+                //       Navigator.push(context, MaterialPageRoute(
+                //         builder: (context) {
+                //           return AuthPage();
+                //         },
+                //       ));
+                //     },
+                //     child: Container(
+                //       margin: EdgeInsets.only(bottom: 25),
+                //       padding:
+                //           EdgeInsets.symmetric(horizontal: 26, vertical: 16),
+                //       decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(25),
+                //         color: Colors.purple,
+                //       ),
+                //       child: Row(
+                //         children: <Widget>[
+                //           Text(
+                //             "START LEARNING",
+                //           ),
+                //           SizedBox(width: 10),
+                //           Icon(
+                //             Icons.arrow_forward,
+                //             color: Colors.black,
+                //           )
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
