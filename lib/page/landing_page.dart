5aa1aee4e28af7f215fb354cf34c68afb55af545
lixklelik <lixkelik@gmail.com>
2022-12-03 23:27:15 +0700

    update

--- lib/page/landing_page.dart
+++ lib/page/landing_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:swaptry/page/auth_page.dart';
 import 'package:swaptry/page/widgets/appTheme.dart';
 
 class LandingPage extends StatefulWidget {
@@ -32,22 +31,23 @@ class _LandingPageState extends State<LandingPage> {
           child: Image.asset('assets/img/logo_black.png'),
         ),
         Container(
-            width: MediaQuery.of(context).size.width,
-            margin:
-                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.42),
-            child: Text(
-              "Welcome to Swaptry",
-              style: textStyle(20, bold, darkerGrey),
-              textAlign: TextAlign.center,
-            )),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
           width: MediaQuery.of(context).size.width,
-          margin:
-              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.47),
-          child: Text("Swap your Electric Motorcycle Battery instantly!",
-              style: textStyle(15, regular, darkerGrey),
-              textAlign: TextAlign.center),
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.42),
+          child: Text(
+            "Welcome to Swaptry",
+            style: textStyle(20, bold, darkerGrey),
+            textAlign: TextAlign.center,
+          )
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.47),
+          child: Text(
+            "Swap your Electric Motorcycle Battery instantly!",
+            style: textStyle(15, regular, darkerGrey),
+            textAlign: TextAlign.center
+          ),
         )
       ]),
     );
