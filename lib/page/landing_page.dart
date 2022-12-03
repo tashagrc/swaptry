1c5fc19aeb72746b59777f5270b97544c1f4d09a
Natasha Grace <natashagradika@gmail.com>
2022-12-03 23:14:20 +0700

    landing

--- lib/page/landing_page.dart
+++ lib/page/landing_page.dart
@@ -14,23 +14,42 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: purple,
-      body: Stack(
-        children:[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 560,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/img/shape.png"),
-                fit: BoxFit.fitWidth,
-              ),
+      body: Stack(children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 560,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/img/shape.png"),
+              fit: BoxFit.fitWidth,
             ),
           ),
-          Container(
-            
-          ), 
-        ]
-      ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          margin:
+              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.2),
+          child: Image.asset('assets/img/logo_black.png'),
+        ),
+        Container(
+            width: MediaQuery.of(context).size.width,
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.42),
+            child: Text(
+              "Welcome to Swaptry",
+              style: textStyle(20, bold, darkerGrey),
+              textAlign: TextAlign.center,
+            )),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          width: MediaQuery.of(context).size.width,
+          margin:
+              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.47),
+          child: Text("Swap your Electric Motorcycle Battery instantly!",
+              style: textStyle(15, regular, darkerGrey),
+              textAlign: TextAlign.center),
+        )
+      ]),
     );
   }
 }
