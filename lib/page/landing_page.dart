58169ba95b61fe74daab986ba0f8bada107186d1
Natasha Grace <natashagradika@gmail.com>
2022-12-03 21:21:39 +0700

    test

--- lib/page/landing_page.dart
+++ lib/page/landing_page.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(110, 128, 254, 1),
+      appBar: AppBar(),
+    );
+  }
+}
